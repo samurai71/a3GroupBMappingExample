e5d55d1b9d57954aa9321bf1205626fe8049fbc4
Landeryou <m_landeryou@tss-hr420-dm40.fc.ca>
2017-02-15 13:12:34 -0500

    combined files

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 (function(){
-	var map, marker;
+	var map =  new google.maps.Map(document.querySelector('.map-wrapper')), marker;
 
-	function initMap() {
-		map = new google.maps.Map(document.querySelector('.map-wrapper'),
-				{
-					center: { lat: 42.983233, lng: -81.250688 },
-					zoom:14
-				}
-			);
+	function initMap(position) {
+		
+		map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+
+		map.setZoom(16);
+
 
 		marker = new google.maps.Marker({
-			position : { lat: 42.983233, lng: -81.250688 },
+			position : { lat: position.coords.latitude, lng: position.coords.longitude },
 			map: map,
 			title: 'Hello World!'
 		});
 	}
 
-	initMap();
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(initMap, handleError);
+	}else {
+		console.log('Your browser does not have geolocation');
+	}
 
+	function handleError(e) {
+		console.log(e)
+;	}
+
+	
 })();
